8be8d6d172a27e64d05452cfb8c4abfb440e571a
Ilya Zaichenko <55659434+DaiFudo@users.noreply.github.com>
2022-05-19 00:05:40 +0000

    questions alpha done

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -2,15 +2,27 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Questions = (props: any) => {
-  //console.log(props.questions);
-  const { questionTitle, questionCode, answers, answer } = props.questions;
+  console.log(props.questions);
+  //const { questionTitle, questionCode, answers, answer } = props.questions;
   return (
-    <>
+    <div>
+      {props.questions.map((item: any) => {
+        return (
+          <>
+            <div>{item.questionTitle}</div>
+            <SyntaxHighlighter language="javascript" style={darcula}>
+              {item.questionCode}
+            </SyntaxHighlighter>
+            <div>Возможные ответы:{item.answers}</div>
+            <details>
+              <summary>Правильный ответ:</summary>
+              <p>{item.answer}</p>
+            </details>
+          </>
+        );
+      })}
       <div className="questionsWrapper"></div>
-      <SyntaxHighlighter language="javascript" style={darcula}>
-        console.log(props.questions);
-      </SyntaxHighlighter>
-    </>
+    </div>
   );
-};
+};;
 export default Questions;

30f9bad5624b3510a0a38e6c28cdc7141e133d87
Ilya Zaichenko <55659434+DaiFudo@users.noreply.github.com>
2022-05-30 21:11:22 +0000

    Work on logic question module

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import "./container.sass";
-const Container = (props: any) => {
+import "../Container/container.css";
+interface ContainerProps {
+  children: React.ReactNode;
+}
+const Container = (props: ContainerProps) => {
   return <div className="container">{props.children}</div>;
 };
 export default Container;
--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,28 +1,67 @@
+import { Radio, RadioChangeEvent } from "antd";
+import { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { notification } from "antd";
+type NotificationType = "success" | "info" | "warning" | "error";
 
 const Questions = (props: any) => {
-  console.log(props.questions);
-  //const { questionTitle, questionCode, answers, answer } = props.questions;
+  const { title, code, answers, key } = props.questions;
+  const [value, setValue] = useState<string>();
+  const [ban, setBan] = useState<boolean>(false);
+  //const [checked, setChecked] = useState<boolean>(false);
+  const onChange = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
+  const winNotify = (type: NotificationType) => {
+    notification[type]({
+      message: `Верно! Ответ: ${key}!`,
+    });
+  };
+  const failNotify = (type: NotificationType) => {
+    notification[type]({
+      message: "Не верно, ответ в подсказке.",
+    });
+  };
+  const congrat = () => {
+    setBan(true);
+    return winNotify("success");
+  };
+  const failed = () => {
+    setBan(true);
+    return failNotify("error");
+  };
+  //console.log(trueAnswer, value);
   return (
     <div>
-      {props.questions.map((item: any) => {
-        return (
-          <>
-            <div>{item.title}</div>
-            <SyntaxHighlighter language="javascript" style={darcula}>
-              {item.code}
-            </SyntaxHighlighter>
-            <div>Возможные ответы:{item.answers}</div>
-            <details>
-              <summary>Правильный ответ:</summary>
-              <p>{item.answer}</p>
-            </details>
-          </>
-        );
-      })}
+      <>
+        <div>{title}</div>
+        <SyntaxHighlighter language="javascript" style={darcula}>
+          {code}
+        </SyntaxHighlighter>
+        {/* <div>Возможные ответы:{item.answers}</div> */}
+        <Radio.Group onChange={onChange} value={value}>
+          {answers.map((text: string) => {
+            return (
+              <Radio
+                onClick={(e) => (key !== e.target.value ? failed() : congrat())}
+                disabled={ban}
+                value={text}
+              >
+                {text}
+              </Radio>
+            );
+          })}
+        </Radio.Group>
+        <details>
+          <summary>Правильный ответ:</summary>
+          <p>{key}</p>
+        </details>
+      </>
       <div className="questionsWrapper"></div>
     </div>
   );
-};;
+};
 export default Questions;

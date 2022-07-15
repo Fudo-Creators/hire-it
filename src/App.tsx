7d0386d3e3236ced9d4653c1730728db0602bb1e
Mikail <gadgihanov5@gmail.com>
2022-07-16 00:48:48 +0300

    task.tsx was changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import styles from './App.module.scss';
 import React from "react";
 
 import { Layout } from "antd";
 
 import Sider from "./components/Sider/Sider";
 import Header from "./components/Header/Header";
+import JsTasks from "./pages/main/js/JsTask/JsTask";
+
+
+
 
 const { Footer, Content } = Layout;
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Sider />
-      <Layout>
-        <Header />
-        {/* <Content>Content</Content> */}
+    <Layout className={styles.wrapp}>
+      <Sider/>
+        <div className="ant-card"></div>
+        {/* <Header /> */}
+        <JsTasks/>
+        <Content>Content</Content>
         {/* <Footer style={{ textAlign: "center" }}>Footer</Footer> */}
-      </Layout>
     </Layout>
   );
 };

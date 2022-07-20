db558002146180a10ec34c3fd1cbb5b1a6ed119d
bxckwood <pumaed2007@gmail.com>
2022-07-20 13:39:54 +0300

    header-tabs

--- src/components/UI/IconsAntD/CheckCircleOutlined.tsx
+++ src/components/UI/IconsAntD/CheckCircleOutlined.tsx
@@ -2,7 +2,7 @@ import { CheckCircleOutlined as AntIcons } from "@ant-design/icons";
 import { IconContextProps } from "@ant-design/icons/lib/components/Context";
 import { FC } from "react";
 
-export const CheckCircleOutlined: FC<IconContextProps> = (props: any) => {
-  return <AntIcons  style={{ color: '#c3e88d' }}  />;
+export const CheckCircleOutlined: FC<IconContextProps> = (props) => {
+  return <AntIcons style={{ color: '#c3e88d' }} {...props} />;
 };
 export default CheckCircleOutlined;

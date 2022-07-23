fbb941f84139a5b3be1dc7d32e0c58ed184806bf
bxckwood <pumaed2007@gmail.com>
2022-07-23 23:12:48 +0300

    dev-question-ilya-sosat+lezhat

--- src/components/UI/IconsAntD/BulbOutlined.tsx
+++
@@ -1,8 +0,0 @@
-import { BulbOutlined as AntIcons } from "@ant-design/icons";
-import { IconContextProps } from "@ant-design/icons/lib/components/Context";
-import { FC } from "react";
-
-export const BulbOutlined: FC<IconContextProps> = (props) => {
-  return <AntIcons style={{ color: "#c3e88d" }} {...props} />;
-};
-export default BulbOutlined;
